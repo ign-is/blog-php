f311baff57d429eff4f81557fc75664c5b94a100
ign-is <igna.dev.contact@gmail.com>
2024-12-30 18:16:41 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog de Videojuegos</title>
+</head>
+<body>
+<!-- CABECERA -->
+    <header>
+        <div id="logo">
+            <a href="index.php">
+                Blog de Videojuegos
+            </a>
+        </div>
+    </header>
+<!-- MENU -->
+<!-- BARRA LATERAL -->
+<!-- CAJA PRINCIPAL -->
+<!-- PUE DE PAGINA -->
+</body>
+</html>
