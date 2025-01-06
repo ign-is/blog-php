6c22d288350dc0590b88e825455d426d75ef5edf
ign-is <igna.dev.contact@gmail.com>
2025-01-06 02:29:37 -0300

    Add pie section

---
+++ includes/pie.php
@@ -0,0 +1,10 @@
+    
+        <div class="clearfix"></div>
+    </div> <!-- fin contenedor -->
+    
+    <!-- PUE DE PAGINA -->
+    <footer id="pie">
+        <p>Desarrollado por Juan Doe &copy; 2024</p>
+     </footer>
+</body>
+</html>
