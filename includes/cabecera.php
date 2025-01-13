d427987daab3b6523113d0c0d0f73667372446de
ign-is <igna.dev.contact@gmail.com>
2025-01-13 03:40:19 -0300

    Add cabecera.php update

--- includes/cabecera.php
+++ includes/cabecera.php
@@ -1,4 +1,6 @@
 <?php require_once 'conexion.php'; ?>
+<?php require_once 'includes/helpers.php'; ?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -22,18 +24,18 @@
                     <li>
                         <a href="index.php">Inicio</a>
                     </li>
-                    <li>
-                        <a href="index.php">Categoria 1</a>
-                    </li>
-                    <li>
-                        <a href="index.php">Categoria 2</a>
-                    </li>
-                    <li>
-                        <a href="index.php">Categoria 3</a>
-                    </li>
-                    <li>
-                        <a href="index.php">Categoria 4</a>
-                    </li>
+                    <?php
+                        $categorias = conseguirCategorias($db); 
+                        if (!empty($categorias)):
+                            while($categoria = mysqli_fetch_assoc($categorias)): 
+                    ?>
+                                <li>
+                                    <a href="categoria.php?id=<?=$categoria['id']?>"><?=$categoria['nombre']?></a>
+                                </li>   
+                    <?php
+                            endwhile; 
+                        endif; 
+                    ?>
                     <li>
                         <a href="index.php">Sobre mi</a>
                     </li>
