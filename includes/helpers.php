828b574b185f243e0642f96ef5df70c0d67f5fc8
ign-is <igna.dev.contact@gmail.com>
2025-01-12 23:26:54 -0300

    Add helpers.php update

--- includes/helpers.php
+++ includes/helpers.php
@@ -13,13 +13,46 @@ function borrarErrores() {
     $borrado = false;
     if (isset($_SESSION['errores'])) {
         $_SESSION['errores'] = null;	
-        unset($_SESSION['errores']);
+        $borrado = true;
     }
+
+    if (isset($_SESSION['errores_entrada'])) {
+        $_SESSION['errores_entrada'] = null;
+        $borrado = true;	
+    }
+
     if (isset($_SESSION['completado'])) {
         $_SESSION['completado'] = null;
-        unset($_SESSION['completado']);
+        $borrado = true;
     }
     return $borrado;
+}
+
+function conseguirCategorias($conexion) {
+
+    $sql = "SELECT * FROM categorias ORDER BY id ASC;";
+    $categorias = mysqli_query($conexion, $sql);
+
+    $resultado = array();
+    if ($categorias && mysqli_num_rows($categorias) >= 1) {
+        $resultado = $categorias;
+    }
+
+    return $resultado;
+}
+
+function conseguirUltimasEntradas($conexion) {
+    $sql = "SELECT e.*, c.nombre AS 'categoria' FROM entradas e ". 
+            "INNER JOIN categorias c ON e.categoria_id = c.id ". 
+            "ORDER BY e.id DESC LIMIT 4";
+    $entradas = mysqli_query($conexion, $sql);
+
+    $resultado = array();
+    if ($entradas && mysqli_num_rows($entradas) >= 1) {
+        $resultado = $entradas;
+    }
+
+    return $entradas;
 }
 
 
