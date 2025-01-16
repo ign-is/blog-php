394c34db248db5d0919f53ce723fda5be0e0a87c
ign-is <igna.dev.contact@gmail.com>
2025-01-15 21:45:45 -0300

    Add helpers.php update

--- includes/helpers.php
+++ includes/helpers.php
@@ -41,10 +41,29 @@ function conseguirCategorias($conexion) {
     return $resultado;
 }
 
-function conseguirUltimasEntradas($conexion) {
+function conseguirCategoria($conexion, $id) {
+
+    $sql = "SELECT * FROM categorias WHERE id = $id;";
+    $categorias = mysqli_query($conexion, $sql);
+
+    $resultado = array();
+    if ($categorias && mysqli_num_rows($categorias) >= 1) {
+        $resultado = mysqli_fetch_assoc($categorias);
+    }
+
+    return $resultado;
+}
+
+function conseguirEntradas($conexion, $limit = null) {
     $sql = "SELECT e.*, c.nombre AS 'categoria' FROM entradas e ". 
             "INNER JOIN categorias c ON e.categoria_id = c.id ". 
-            "ORDER BY e.id DESC LIMIT 4";
+            "ORDER BY e.id DESC";
+    
+     if ($limit) {
+        // $sql = #sql." LIMIT 4"
+        $sql .= " LIMIT 4";
+    }
+
     $entradas = mysqli_query($conexion, $sql);
 
     $resultado = array();
